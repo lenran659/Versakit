138ef554f6cf0310f14be77d94bc5d8d700c6dbd
裴雨孜 <2171204141@qq.com>
2024-12-25 21:29:02 +0800

    feat: vitepress 暗黑模式切换、适配暗黑模式样式

--- packages/docs/.vitepress/theme/index.ts
+++ packages/docs/.vitepress/theme/index.ts
@@ -3,9 +3,11 @@ import Versakit from '@versakit/ui' // 引入组件库
 import '@versakit/ui/dist/style.css' // 引入样式
 import './index.scss' // 引入样式
 import Tool from '../../example/tool.vue'
+import MyLayout from './Layout.vue'
 
 export default {
   ...Theme,
+  Layout: MyLayout,
   enhanceApp({ app }) {
     app.component('Tool', Tool)
     app.use(Versakit) // 注册组件库
