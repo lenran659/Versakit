c6edec657e66bf735e4e3c4ca695613c3aedebce
Jannik <1337741710@qq.com>
2025-01-08 21:44:33 +0800

    docs: 文档更新

--- packages/docs/example/switch/size.vue
+++ packages/docs/example/switch/size.vue
@@ -1,16 +1,23 @@
 <template>
   <div>
-    <VerSwitch size="lg" v-model="value" />
-    <VerSwitch v-model="value" />
-    <VerSwitch size="sm" v-model="value" />
+    <VerSegmented v-model="selectedValue" :options="options"></VerSegmented>
+
+    <VerSwitch :size="selectedValue" v-model="value" />
   </div>
 </template>
 
 <script lang="ts" setup>
-import { VerSwitch } from '@versakit/ui'
+import { VerSwitch, VerSegmented } from '@versakit/ui'
 import { ref } from 'vue'
 
 const value = ref(false)
+const selectedValue = ref('md')
+
+const options = [
+  { label: 'SM', value: 'sm' },
+  { label: 'MD', value: 'md' },
+  { label: 'LG', value: 'lg' },
+]
 </script>
 
 <style scoped>
