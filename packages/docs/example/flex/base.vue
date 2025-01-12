b7d5d8570216061ceb194c2e25cd5667c01b3226
zhuoyixin <1454047497@qq.com>
2025-01-12 21:40:21 +0800

    feat: Flex弹性布局

---
+++ packages/docs/example/flex/base.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { VerFlex, VerRadio } from '@versakit/ui'
+
+const direction = ref('horizontal')
+</script>
+
+<template>
+  <div>
+    <VerRadio label="horizontal" v-model="direction">horizontal</VerRadio>
+    <VerRadio label="vertical" v-model="direction">vertical</VerRadio>
+  </div>
+  <VerFlex :vertical="direction === 'vertical'">
+    <div v-for="n in 4" :key="n" class="flex-item" />
+  </VerFlex>
+</template>
+
+<style scoped>
+.flex-item {
+  width: 25%;
+  height: 80px;
+  background-color: #c4b5fd;
+}
+</style>
---
+++ packages/docs/example/flex/gap.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { ref, computed } from 'vue'
+import { VerRadio, VerSlider, VerFlex } from '@versakit/ui'
+
+const gapSize = ref<'small' | 'middle' | 'large' | 'custom'>('small')
+const customGapSize = ref(16)
+
+const computedGap = computed<number | 'small' | 'middle' | 'large'>(() => {
+  if (gapSize.value === 'custom') {
+    return customGapSize.value
+  }
+  return gapSize.value
+})
+</script>
+<template>
+  <div>
+    <VerRadio label="small" v-model="gapSize">small</VerRadio>
+    <VerRadio label="middle" v-model="gapSize">middle</VerRadio>
+    <VerRadio label="large" v-model="gapSize">large</VerRadio>
+    <VerRadio label="custom" v-model="gapSize">custom</VerRadio>
+    <VerSlider
+      v-model="customGapSize"
+      v-show="gapSize === 'custom'"
+    ></VerSlider>
+  </div>
+  <VerFlex :gap="computedGap" style="margin-top: 10px">
+    <div v-for="n in 4" :key="n" class="flex-item" />
+  </VerFlex>
+</template>
+
+<style scoped>
+.flex-item {
+  width: 80px;
+  height: 40px;
+  background-color: #c4b5fd;
+}
+</style>
